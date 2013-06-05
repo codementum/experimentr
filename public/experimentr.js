84332f7416ffb11bbb36c6b241db10eccbe14f72
Lane Harrison <lanetharrison@gmail.com>
2013-06-05 10:51:25 -0400

    added next button and function. removed module loading so I can make it load them sequentially (instead of all at once) later.

--- public/experimentr.js
+++ public/experimentr.js
@@ -5,23 +5,44 @@ experimentr = function() {
 
   var sequence;
 
+  var mainDiv, moduleDiv, controlDiv;
+
   experimentr.start = function() {
     console.log('starting experiment sequence: ' + sequence);
-    var mainDiv = d3.select('body').append('div')
-      .attr('id', 'experimentr');
-    var expDiv = mainDiv.selectAll('.experimentr-section')
-      .data(sequence)
-      .enter().append('div')
-      .classed('experimentr-section', true)
-      .each(loadHTML);
-    function loadHTML() {
-      var div = d3.select(this);
-      d3.html(div.datum(), function(err, d) {
-        if(err) console.log(err);
-        div.node().appendChild(d);
-      });
-    }
+    init();
   };
+
+  function init() {
+    mainDiv = d3.select('body').append('div')
+      .attr('id', 'experimentr');
+    moduleDiv = mainDiv.append('div')
+      .attr('id', 'module');
+    controlDiv = mainDiv.append('div')
+      .attr('id', 'control');
+    controlDiv.append('button')
+      .attr('type', 'button')
+      .text('Next')
+      .on('click', function() { experimentr.next(); });
+  }
+
+  // // TODO this isn't right. They should be built as needed.
+  // var moduleDiv = mainDiv.selectAll('.experimentr-section')
+  //   .data(sequence)
+  //   .enter().append('div')
+  //   .classed('experimentr-section', true)
+  //   .each(loadHTML);
+
+  function loadHTML() {
+    var div = d3.select(this);
+    d3.html(div.datum(), function(err, d) {
+      if(err) console.log(err);
+      div.node().appendChild(d);
+    });
+  }
+
+  experimentr.next = function() {
+    console.log('next');
+  }
 
   experimentr.sequence = function(x) {
     if(!arguments.length) return sequence;
