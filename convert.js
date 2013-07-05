e544cacc3adf789a957ef3be1ef52de2b5b1ad79
Lane Harrison <lanetharrison@gmail.com>
2013-07-05 18:19:57 +0000

    convert a json file given fields

---
+++ convert.js
@@ -0,0 +1,30 @@
+var j2c    = require('json2csv')
+  , fs     = require('fs')
+  , file   = process.argv[2]
+  , _      = require('underscore')
+  , fields = ['postId', 'preValence', 'postValence']
+  , data
+
+fs.readFile(file, 'utf8', function (err, data) {
+  if (err) console.log(err)
+  data = JSON.parse(data)
+  data = filterUndefined(data)
+  convert( data )
+})
+
+function convert(d) {
+  var params = {
+    data: d,
+    fields: fields
+  }
+  j2c(params, function(err, csv) {
+    if (err) console.log(err)
+    console.log(csv)
+  })
+}
+
+function filterUndefined (arr) {
+  return _.filter(arr, function(row) {
+    return _.every(fields, function(f) { return row[f] })
+  })
+}
