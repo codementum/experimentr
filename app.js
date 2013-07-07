2a3264b42d7393120396575852956b5597401338
Lane Harrison <lanetharrison@gmail.com>
2013-07-07 15:42:34 +0000

    added default port and option to set from cli

--- app.js
+++ app.js
@@ -1,52 +1,45 @@
 /* global require:true, console:true, process:true, __dirname:true */
-'use strict';
+'use strict'
 
 var express     = require('express')
   , http        = require('http')
   , fs          = require('fs')
+  , port        = process.argv[2] || 8000
   , redis       = require('redis')
   , redisClient
-  , dataDir     = 'userdata/'
-  , port        = '8000';
 
-// setup for redis
-redisClient = redis.createClient();
+redisClient = redis.createClient()
 
-// redis connection test
 redisClient.on('connect', function() {
-  console.log('Connected to redis.');
-});
+  console.log('Connected to redis.')
+})
 
 // CORS middleware
 var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Length, Content-Type, Date');
-  next();
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Length, Content-Type, Date')
+  next()
 }
 
-// Init
-var app = express();
-app.use(express.bodyParser());
-app.use(allowCrossDomain);
-app.use(express.static(__dirname + '/public'));
+var app = express()
+app.use(express.bodyParser())
+app.use(allowCrossDomain)
+app.use(express.static(__dirname + '/public'))
 
-// POST
 app.post('/', function handlePost(req, res) {
-  var d = req.body;
-  if (!d.postId) d.postId = (+new Date()).toString(36);
-  d.timestamp = (new Date()).getTime();
-  saveRedis(d);
-  res.send(200);
+  var d = req.body
+  if (!d.postId) d.postId = (+new Date()).toString(36)
+  d.timestamp = (new Date()).getTime()
+  saveRedis(d)
+  res.send(200)
 })
 
 var saveRedis = function saveRedis(d) {
-  redisClient.hmset(d.postId, d);
-  console.log('saved to redis: ' + d.postId +', at: '+ (new Date()).toString());
+  redisClient.hmset(d.postId, d)
+  console.log('saved to redis: ' + d.postId +', at: '+ (new Date()).toString())
 }
 
 http.createServer(app).listen(port, function (err) {
-  if (!err) {
-    console.log('Listening on port ' + port);
-  }
-});
+  if (!err) console.log('Listening on port ' + port)
+})
