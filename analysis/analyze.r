65c5699211f9571a0d5467229c0da4ab7cdf02d4
Lane Harrison <lanetharrison@gmail.com>
2013-07-07 15:37:53 +0000

    more R commands

--- analysis/analyze.r
+++ analysis/analyze.r
@@ -1,6 +1,6 @@
 d = read.csv('data.csv', header = TRUE, sep = ",")
 
-# long method (will need for multiple charts)
+# long method (can use to filter by chart type)
 # n = d$primingType == 'negative' 
 # p = d$primingType == 'positive' 
 # d2.n = d[n,]$cm_average 
@@ -9,3 +9,20 @@ d = read.csv('data.csv', header = TRUE, sep = ",")
 
 # short method
 t.test(cm_average ~ primingType, data=d)
+
+# ANOVA: http://www.statmethods.net/stats/anova.html
+# One Way Anova (Completely Randomized Design)
+#fit <- aov(cm_average ~ chart, data=d)
+#summary(fit)
+# Plots
+#layout(matrix(c(1,2,3,4),2,2)) # optional layout 
+#plot(fit) # diagnostic plots
+# Tukey Honestly Significant Differences
+# TukeyHSD(fit) # where fit comes from aov()
+
+# Plot Means with Error Bars
+# library(gplots)
+# attach(d)
+# primingType <- factor(primingType)
+# plotmeans(cm_average ~ primingType,xlab="priming type",
+#             ylab="error cm", main="Mean Plot\nwith 95% CI")
