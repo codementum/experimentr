9aa85d490e363a0bf2798fb63fe3404edb9f2f67
Lane Harrison <lanetharrison@gmail.com>
2013-07-08 15:11:03 +0000

    pull time

--- analysis/convert.js
+++ analysis/convert.js
@@ -2,7 +2,7 @@ var j2c    = require('json2csv')
   , fs     = require('fs')
   , file   = process.argv[2]
   , _      = require('underscore')
-  , fields = ['workerId', 'postId', 'primingType', 'successfulPrime', 'valenceDiff', 'cm_average', 'chart']
+  , fields = ['workerId', 'postId', 'primingType', 'successfulPrime', 'valenceDiff', 'cm_average', 'chart', 'time_diff_average']
   , data
 
 fs.readFile(file, 'utf8', function (err, data) {
