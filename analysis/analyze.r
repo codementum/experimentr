31bf9c6a7bc6d4b31e390bb96c0bd71b57f71c4d
Lane Harrison <lanetharrison@gmail.com>
2013-07-09 18:35:53 +0000

    more metrics to look at; filtering bad participants

--- analysis/analyze.r
+++ analysis/analyze.r
@@ -3,30 +3,49 @@ sink('results.txt', append=FALSE, split=FALSE)
 
 d = read.csv('data.csv', header = TRUE, sep = ",")
 
+cat('All charts:\n\n\n')
+t.test(cm_average ~ primingType, data=d)
+
+cat('All participants:\n\n\n')
+
 cat('adjacent:\n')
 adj = subset(d, chart == 'bar-adjacent')
 summary(adj$primingType)
 t.test(cm_average ~ primingType, data=adj)
 
-cat('nonAdjacentOne:\n')
-one = subset(d, chart == 'bar-nonAdjacentOne')
-summary(one$primingType)
-t.test(cm_average ~ primingType, data=one)
-
-cat('nonAdjacentThree:\n')
-three = subset(d, chart == 'bar-nonAdjacentThree')
-summary(three$primingType)
-t.test(cm_average ~ primingType, data=three)
+#cat('nonAdjacentOne:\n')
+#one = subset(d, chart == 'bar-nonAdjacentOne')
+#summary(one$primingType)
+#t.test(cm_average ~ primingType, data=one)
+#
+#cat('nonAdjacentThree:\n')
+#three = subset(d, chart == 'bar-nonAdjacentThree')
+#summary(three$primingType)
+#t.test(cm_average ~ primingType, data=three)
 
 cat('nonAdjacentFive:\n')
 five = subset(d, chart == 'bar-nonAdjacentFive')
 summary(five$primingType)
 t.test(cm_average ~ primingType, data=five)
 
-cat('nonAdjacentSeven:\n')
-seven = subset(d, chart == 'bar-nonAdjacentSeven')
-summary(seven$primingType)
-t.test(cm_average ~ primingType, data=seven)
+
+cat('Primed participants:\n\n\n')
+
+cat('adjacent:\n')
+padj = subset(adj, successfulPrime == 'true')
+summary(padj$primingType)
+t.test(cm_average ~ primingType, data=padj)
+
+cat('nonAdjacentFive:\n')
+pfive = subset(five, successfulPrime == 'true')
+summary(pfive$primingType)
+t.test(cm_average ~ primingType, data=pfive)
+
+
+#cat('nonAdjacentSeven:\n')
+#seven = subset(d, chart == 'bar-nonAdjacentSeven')
+#summary(seven$primingType)
+#t.test(cm_average ~ primingType, data=seven)
 
 cat('task time:\n')
 t.test(time_diff_average ~ primingType, data=d)
@@ -34,8 +53,17 @@ t.test(time_diff_average ~ primingType, data=d)
 cat('sd cm_average:\n')
 sd(d$cm_average)
 
+cat('mean cm_average:\n')
+mean(d$cm_average)
+
 cat('sd time_diff_average:\n')
 sd(d$time_diff_average)
+
+cat('sd time_diff_experiment:\n')
+sd(d$time_diff_experiment)
+
+cat('mean time_diff_experiment:\n')
+mean(d$time_diff_experiment)
 
 cat('priming summary:\n')
 summary(d$primingType)
--- analysis/convert.js
+++ analysis/convert.js
@@ -2,13 +2,17 @@ var j2c    = require('json2csv')
   , fs     = require('fs')
   , file   = process.argv[2]
   , _      = require('underscore')
-  , fields = ['workerId', 'postId', 'primingType', 'successfulPrime', 'valenceDiff', 'cm_average', 'chart', 'time_diff_average']
+  , fields = ['workerId', 'postId', 'primingType', 'successfulPrime', 'valenceDiff', 'cm_average', 'chart', 'time_diff_average', 'time_diff_experiment']
   , data
 
 fs.readFile(file, 'utf8', function (err, data) {
   if (err) console.log(err)
   data = JSON.parse(data)
   data = filterUndefined(data)
+  data = filterDebug(data)
+//  data = filterBaddies(data)
+  data = filterTime(data)
+  data = filterError(data)
   convert( data )
 })
 
@@ -26,5 +30,44 @@ function convert(d) {
 function filterUndefined (arr) {
   return _.filter(arr, function(row) {
     return _.every(fields, function(f) { return row[f] })
+  })
+}
+
+function filterDebug (arr) {
+  return _.filter(arr, function(row) {
+    return row.workerId !== 'debug'
+  })
+}
+
+function filterBaddies (arr) {
+var baddies = [
+  'hixc8a98',
+  'hixaksbn',
+  'hixa8w5u',
+  'A2E72ZI1VESTMR',
+  'hixav7bf',
+  'hixa5i8t',
+  'hixap19x',
+  'Link wont work!',
+  'no survey'
+]
+  return _.filter(arr, function(row) {
+    return ! _.contains(baddies, row.postId)
+  })
+}
+
+function filterTime (arr) {
+  return _.filter(arr, function(row) {
+    var low = 328898 - 129767*2
+    var high = 328898 + 129767*2
+    return row.time_diff_experiment < high && row.time_diff_experiment > low
+  })
+}
+
+function filterError (arr) {
+  return _.filter(arr, function(row) {
+    var low = 2.46 - 1.18*2
+    var high = 2.46 + 1.18*2
+    return row.cm_average < high && row.cm_average > low
   })
 }
