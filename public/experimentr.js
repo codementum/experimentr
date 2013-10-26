98fe0cfd4cb87e8b01d5fccd396b201351ec5e46
Lane Harrison <lanetharrison@gmail.com>
2013-10-26 14:45:03 -0400

    Added documentation to experimentr.js. Fixes #46. :shipit:

--- public/experimentr.js
+++ public/experimentr.js
@@ -1,23 +1,23 @@
 experimentr = function() {
-  var experimentr = {
-    version: "0.0.1"
-  };
+  var experimentr = { version: "0.0.1" }
+    , sequence
+    , current
+    , mainDiv
+    , data = {};
 
-  var sequence;
-  var current;
-  var mainDiv;
-  var data = {};
-
+  // Add a random postId for each new participant
   data.postId = (+new Date()).toString(36);
 
+  // Accessor for postId
   experimentr.postId = function() {
     return data.postId;
   };
-
+  // Accessor for data
   experimentr.data = function() {
     return data;
   };
 
+  // Starts the experiment by loading the first module
   experimentr.start = function() {
     init();
     current = 0;
@@ -25,10 +25,8 @@ experimentr = function() {
     experimentr.startTimer('experiment');
   };
 
-  experimentr.clearNext = function(cb) {
-    d3.select('#next-button').on('click', experimentr.next);
-  };
-
+  // Add a function to be called when a person clicks next.
+  // (This can be cleared with clearNext()).
   experimentr.onNext = function(cb) {
     d3.select('#next-button').on('click', function() {
       cb();
@@ -36,6 +34,13 @@ experimentr = function() {
     });
   };
 
+  // Reset the callbacks on next click.
+  experimentr.clearNext = function() {
+    d3.select('#next-button').on('click', experimentr.next);
+  };
+
+
+  // Create the divs for the experiment content and controls.
   function init() {
     if(mainDiv) return;
     mainDiv = d3.select('body').append('div')
@@ -52,6 +57,7 @@ experimentr = function() {
         .on('click', experimentr.next);
   }
 
+  // Load the next module.
   experimentr.next = function() {
     experimentr.clearNext();
     experimentr.showNext();
@@ -59,15 +65,18 @@ experimentr = function() {
     activate(current);
   }
 
+  // This just ends the experiment timer right now, but it might be a good place to send final experiment data (if we are using CSV on the backend).
   experimentr.end = function() {
     experimentr.endTimer('experiment');
   }
 
+  // Adds the data in `d` to the experiment data, and saves to server.
   experimentr.addData = function(d) {
     merge(data, d);
     experimentr.save();
   }
 
+  // The HTTP POST code for saving experiment data.
   experimentr.save = function(d) {
     d3.xhr('/')
       .header("Content-Type", "application/json")
@@ -76,30 +85,37 @@ experimentr = function() {
       });
   }
 
+  // Merges object o2 into o1.
   function merge(o1, o2) {
     for (var attr in o2) { o1[attr] = o2[attr]; }
   }
 
+  // Enables the Next button so the user can proceed in the experiment.
   experimentr.release = function() {
     d3.select('#next-button').attr('disabled', null);
   }
 
+  // On some multi-part modules, it is helpful to hide the next button until it is needed.
   experimentr.hideNext = function() {
     d3.select('#next-button').style('display', 'none');
   }
 
+  // Make the Next button visible.
   experimentr.showNext = function() {
     d3.select('#next-button').style('display', 'inline');
   }
 
+  // Disables the Next button until the module code decides the user is done with the current module (release by experimentr.release()).
   experimentr.hold = function() {
     d3.select('#next-button').attr('disabled', true);
   }
 
+  // Remove experiment content from the main div.
   function clearModule() {
     d3.select('#module').html('');
   }
 
+  // Activate (load) a given module.
   function activate(x) {
     clearModule();
     experimentr.hold();
@@ -115,21 +131,26 @@ experimentr = function() {
     });
   }
 
+  // Remove the next button entirely (TODO ensure this is actually used).
   function removeNextButton() {
     d3.select('#next-button').remove();
   }
 
+  // Define the experiment sequence via an array (see index.html for an example).
   experimentr.sequence = function(x) {
     if(!arguments.length) return sequence;
     sequence = x;
     return experimentr;
   }
 
+  // Start a timer with a given String as key
   experimentr.startTimer = function(x) {
     console.log('starting timer: '+x);
     data['time_start_'+x] = Date.now(); 
   }
 
+  // End an existing timer (using a String key)
+  // TODO throw an error if a start wasn't called.
   experimentr.endTimer = function(x) {
     console.log('ending timer: '+x);
     data['time_end_'+x] = Date.now(); 
@@ -137,5 +158,6 @@ experimentr = function() {
     experimentr.save();
   }
 
+  // Returns experimentr so we can use it in index.html
   return experimentr;
 }();
